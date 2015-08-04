318945ceff24a87697170b4914b861729b862395
Peter Henry <phenry@caltech.edu>
2015-08-04 15:14:24 -0600

    changed from function to class based views

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from main import views
+
 urlpatterns = [
-    url(r'^snippets/$', 'main.views.snippet_list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', 'main.views.snippet_detail'),
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
