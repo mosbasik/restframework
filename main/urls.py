391104f2d7df3ca256add0f9560ee5af06d36a35
Peter Henry <phenry@caltech.edu>
2015-08-04 14:56:36 -0600

    completed tutorial #2; working extensions and browser interface

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^snippets/$', 'main.views.snippet_list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', 'main.views.snippet_detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
