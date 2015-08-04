b3dd0eae2716396b9be0d5ab9b366e5579c1fddd
Peter Henry <phenry@caltech.edu>
2015-08-04 11:14:46 -0600

    Snippet model created and database configured

--- project/settings.py
+++ project/settings.py
@@ -33,8 +33,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
     'django.contrib.humanize',
+    'rest_framework',
+    'main',
 )
 
 MIDDLEWARE_CLASSES = (
