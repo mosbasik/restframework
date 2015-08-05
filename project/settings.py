cb5e268b85044222225ed5fb3c12c7c4dba9ec69
Peter Henry <phenry@caltech.edu>
2015-08-05 12:51:33 -0600

    added rest framework default pagination

--- project/settings.py
+++ project/settings.py
@@ -93,4 +93,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 2
+}
+
 from settings_secret import *
